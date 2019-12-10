4ed13a9c719d5bcb37a13929f64b94a6856c4fea
jemm <jemm@itu.dk>
2019-12-10 15:07:27 +0100

    Added method to create user and with it a document containing information about user

--- src/Model/Account.java
+++ src/Model/Account.java
@@ -1,20 +1,41 @@
 package Model;
 
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Account {
     protected String username;
     protected String password;
-    protected int age;
+    protected String email;
     protected ArrayList<Media> myList;
 
 
-    public Account(String username, String password, int age){
+    public Account(String username, String email,String password){
         this.username = username;
+        this.email = email;
         this.password = password;
-        this.age = age;
-        myList = new ArrayList<>();
+        this.myList = new ArrayList<Media>();
+        createAccountFile();
     }
 
+    public void loadMyList(String email) {
+        this.myList = new ArrayList<>();
+    }
+
+    public void createAccountFile(){
+        try {
+            PrintWriter writer = new PrintWriter("./Data/Accounts/"+this.email+".txt", StandardCharsets.ISO_8859_1);
+            writer.println(this.username+";"+this.email+";"+this.password);
+            for (Media m:myList) {
+                writer.print(m.title+";");
+            }
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     public void addMyList(Media m){
         myList.add(m);
     }
