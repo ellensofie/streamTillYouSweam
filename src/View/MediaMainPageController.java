a41e5c758647cc7caeba25c9b14b04c2edbe10bd
Ellen-Sofie <elch@itu.dk>
2019-12-11 17:23:20 +0100

    Series lavet dog inlæser den film istedet for serier

--- src/View/MediaMainPageController.java
+++ src/View/MediaMainPageController.java
@@ -246,6 +246,16 @@ public class MediaMainPageController implements Initializable {
         stage.setScene(scene);
         stage.show();
     }
+
+    public void btSeries() throws IOException {
+        Stage stage = (Stage) btSeries.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("Series.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
 
 
